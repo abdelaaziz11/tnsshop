c69a95a65ab780e8db7ea9276155ddb5745c580e
abdelaaziz khouda <abdelaazizkhouda@abdelaazizs-MacBook-Air.local>
2025-01-21 23:32:51 +0100

    tns shop ecommerce app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import logo from '../Assets/Frontend_Assets/logo.png'
+import './Navbar.css'
+import cart from '../Assets/Frontend_Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+    <div className='Navbar'>
+      <div className='Nav'>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <p>TNS Shop</p>
+        </div>
+        <div className="nav-items">
+          <ul className='center-items'>
+            <li><a href="/store">Store</a></li>
+            <li><a href="/male">Male</a></li>
+            <li><a href="/women">Female</a></li>
+          </ul>
+          <ul className='right-items'>
+            <li>
+              <a href="/login"><button className="btn login-btn">Login</button></a>
+            </li>
+            <li>
+              <img src={cart} alt="cart" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
