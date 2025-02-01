04690fd01388c99ee7ae7f1814da9bb06f75073f
abdelaaziz khouda <abdelaazizkhouda@abdelaazizs-MacBook-Air.local>
2025-02-01 14:01:54 +0100

    tns shop ecommerce app front end done

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import Breadcrums from '../Components/Breadcrums/Breadcrums'
+import { ShopContext } from '../Context/ShopContext'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts'
 
-const Product = () => {
+const Product = () => { 
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams()
+  const product = all_product?.find((e)=> e.id === Number(productId))
+  if (!product) {
+    return <div>Product not found</div>;
+  }
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrums product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts/>
+    </div>
   )
 }
 
