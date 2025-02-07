1f560878401835a59ff81d943824af988e6db731
abdelaaziz khouda <abdelkhouda055@gmail.com>
2025-02-07 12:15:21 +0100

    add the backend with mongodb

---
+++ tns-backed/index.js
@@ -0,0 +1,30 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// Database conection with mongoDB 
+mongoose.connect("mongodb+srv://abdelaaziz:019020@cluster0.selrp.mongodb.net/tns-shop");
+// API creation
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+
+app.listen(port,(error)=>{
+    if (!error) {
+        console.log("Server Running on Port " + port)
+    }
+    else {
+        console.log("Error :"+error)
+    }
+})
+
